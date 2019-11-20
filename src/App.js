1c9611ec0fa9484ec917969d72ff9f2aa33e52aa
grayluker05 <graysonluker@gmail.com>
2019-11-20 11:42:00 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {library} from '@fortawesome/fontawesome-svg-core';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faBars} from '@fortawesome/free-solid-svg-icons';
+library.add(faBars);
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header id="header-container">
+        <img 
+          src="https://i.kym-cdn.com/entries/icons/facebook/000/003/047/omg.jpg" 
+          alt="handsome-squidward" 
+          id="logo"
+        />
+        <nav id="nav-links">
+        <a>Home</a>
+        <a>About</a>
+        <a>Contact</a>
+        </nav>
+        <FontAwesomeIcon icon="bars" id="hamburger"/>
       </header>
+      <div id="rotate-box"></div>
+      <div id="transition-box"></div>
     </div>
   );
 }
