71d0fe17505d5cdf9c55691ff4325fa4ba0139c5
grayluker05 <graysonluker@gmail.com>
2019-11-20 12:14:03 -0700

    good notes for css

--- src/App.js
+++ src/App.js
@@ -1,30 +1,68 @@
-import React from 'react';
+import React,{Component} from 'react';
 import './App.css';
 import {library} from '@fortawesome/fontawesome-svg-core';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import {faBars} from '@fortawesome/free-solid-svg-icons';
 library.add(faBars);
 
-function App() {
-  return (
-    <div className="App">
-      <header id="header-container">
-        <img 
-          src="https://i.kym-cdn.com/entries/icons/facebook/000/003/047/omg.jpg" 
-          alt="handsome-squidward" 
-          id="logo"
-        />
-        <nav id="nav-links">
-        <a>Home</a>
-        <a>About</a>
-        <a>Contact</a>
-        </nav>
-        <FontAwesomeIcon icon="bars" id="hamburger"/>
-      </header>
-      <div id="rotate-box"></div>
-      <div id="transition-box"></div>
-    </div>
-  );
+class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      toggleMenu: false
+    }
+  }
+
+  handleToggle = () => {
+    this.setState({
+      toggleMenu: !this.state.toggleMenu
+    })
+  }
+
+  render(){
+    return (
+      <div className="App">
+        {!this.state.toggleMenu
+        ? (<header id="header-container">
+         <img 
+            src="https://i.kym-cdn.com/entries/icons/facebook/000/003/047/omg.jpg" 
+            alt="handsome-squidward" 
+            id="logo"
+          />
+          <nav id="nav-links">
+            <a>Home</a>
+            <a>About</a>
+            <a>Contact</a>
+         </nav>
+          <FontAwesomeIcon icon="bars" id="hamburger" onClick={this.handleToggle}/>
+        </header>)
+        : (
+          <>
+          <header id="header-container">
+            <img 
+              src="https://i.kym-cdn.com/entries/icons/facebook/000/003/047/omg.jpg" 
+              alt="handsome-squidward" 
+              id="logo"
+            />
+            <nav id="nav-links">
+              <a>Home</a>
+              <a>About</a>
+              <a>Contact</a>
+            </nav>
+            <FontAwesomeIcon icon="bars" id="hamburger" onClick={this.handleToggle}/>
+          </header>
+          <nav id="side-menu">
+            <a>Home</a>
+            <a>About</a>
+            <a>Contact</a>
+          </nav>
+        </>)
+        }
+       <div id="rotate-box"></div>
+        <div id="transition-box"></div>
+      </div>
+    );
+  }
 }
 
 export default App;
